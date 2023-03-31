f5ebc3f9f53a0fdca7ffbfa1574d310b77d3ff48
Kelvin Alef <kelvin.alefs@gmail.com>
2023-03-31 20:23:14 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Esta pagina e um exercio do programa IT Talent DevOps Básico.
         </p>
         <a
           className="App-link"
