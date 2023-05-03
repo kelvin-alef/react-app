2b4b64730c953ed2aad31d735b73886a833dee12
Caio Vinicius <106553681+caio-vmelo@users.noreply.github.com>
2023-05-03 14:44:13 -0300

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn devops/i);
+  const linkElement = screen.getByText(/Learn DevOps - CI/CD/i);
   expect(linkElement).toBeInTheDocument();
 });
