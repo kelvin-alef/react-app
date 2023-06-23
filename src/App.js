21a07bae5161d92958d718da8e1b474742a92404
Kelvin Alef <kelvin.alefs@gmail.com>
2023-06-23 09:28:01 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          DevOps Básico IT Talent - Mudanças aula 26-04-2023
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn DevOps
+          Learn React
         </a>
       </header>
     </div>
