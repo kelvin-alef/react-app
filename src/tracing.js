fb7ce0c4d5f321d0da1297cff1fa357467703713
Kelvin Alef <kelvin.alefs@gmail.com>
2023-07-03 17:54:53 -0300

    Create tracing.js

---
+++ src/tracing.js
@@ -0,0 +1,18 @@
+import { WebTracerProvider } from '@opentelemetry/sdk-trace-web';
+import { ConsoleSpanExporter, SimpleSpanProcessor } from '@opentelemetry/tracing';
+import { ZipkinExporter } from '@opentelemetry/exporter-zipkin';
+
+const provider = new WebTracerProvider();
+
+// Configurar o exportador para o Zipkin
+const exporter = new ZipkinExporter({
+  serviceName: 'react-app', // Nome da sua aplicação
+  url: 'http://localhost:9411/api/v2/spans', // Endereço do servidor Zipkin
+});
+
+// Adicionar exportadores e processadores de spans
+provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
+provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
+
+// Registrar o provedor globalmente
+provider.register();
