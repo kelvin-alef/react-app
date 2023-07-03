6d9902f37e9aedd65e45a89d4eddf042123b41ae
Kelvin Alef <kelvin.alefs@gmail.com>
2023-07-03 17:33:08 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,36 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-if (!globalThis.os) {
-  globalThis.os = require('os-browserify/browser');
-}
-
-import { initTracer } from 'jaeger-client';
-
-const tracer = initTracer({ serviceName: 'react-app' });
-
-const makeTracedRequest = async (method, url, config) => {
-  const span = tracer.startSpan(method.toUpperCase());
-  span.setTag('http.url', url);
-  span.setTag('http.method', method.toUpperCase());
-
-  try {
-    const response = await fetch(url, config);
-    span.setTag('http.status_code', response.status);
-    return response;
-  } catch (error) {
-    span.setTag('error', true);
-    throw error;
-  } finally {
-    span.finish();
-  }
-};
-
-const http = {
-  get: (url, config) => makeTracedRequest('GET', url, config),
-  post: (url, data, config) => makeTracedRequest('POST', url, { ...config, body: JSON.stringify(data) })
-};
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
