e61edc8cf0ebf93636dc9d9b62ca3c13d0dd381d
Kelvin Alef <kelvin.alefs@gmail.com>
2023-07-03 14:12:10 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const tracer = initTracer(config);
+opentracing.initGlobalTracer(tracer);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
 // jaeger
 import { initTracer } from 'jaeger-client';
 import opentracing from 'opentracing';
@@ -20,18 +35,3 @@ const config = {
     agentPort: 6831, // Substitua pelo número da porta do serviço Jaeger
   },
 };
-
-const tracer = initTracer(config);
-opentracing.initGlobalTracer(tracer);
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
