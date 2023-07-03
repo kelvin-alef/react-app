610ac115caa169e8a6a3b2e8d29879141dd88461
Kelvin Alef <kelvin.alefs@gmail.com>
2023-07-03 18:43:35 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { init as initApm } from '@elastic/apm-rum';
+
+const apm = initApm({
+  serviceName: 'react-app',
+  serverUrl: 'http://localhost:8200',
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
