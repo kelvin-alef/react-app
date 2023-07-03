4f37b6b3009fbeb261252244a6cafa915bd8a43a
Kelvin Alef <kelvin.alefs@gmail.com>
2023-07-03 15:35:11 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'os-browserify/browser'; 
 import http from './tracing';
+
+const { execSync } = require('child_process');
+execSync('npm install os-browserify');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,7 +15,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
