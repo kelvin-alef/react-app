b82f592715a1804783ec0fac8662935df03b9bfd
Kelvin Alef <kelvin.alefs@gmail.com>
2023-07-03 15:15:15 -0300

    Update tracing.js

--- src/tracing.js
+++ src/tracing.js
@@ -1,32 +1,31 @@
+import axios from 'axios';
 import { initTracer } from 'jaeger-client';
 
-const serviceName = 'react-app';
-const samplerConfig = {
-  type: 'const',
-  param: 1,
-};
+const tracer = initTracer({ serviceName: 'react-app' });
 
-const reporterConfig = {
-  logSpans: true,
-};
+const http = axios.create();
+http.interceptors.request.use(config => {
+  const span = tracer.startSpan(config.method.toUpperCase());
+  span.setTag('http.url', config.url);
+  span.setTag('http.method', config.method.toUpperCase());
+  span.setTag('http.status_code', 200);  // Exemplo: definir o status code da resposta
+  config.headers['X-Trace-ID'] = span.context().traceIdStr;
+  return config;
+});
 
-const config = {
-  serviceName,
-  sampler: samplerConfig,
-  reporter: reporterConfig,
-};
-
-const options = {
-  logger: {
-    info(msg) {
-      console.log('INFO', msg);
-    },
-    error(msg) {
-      console.log('ERROR', msg);
-    },
+http.interceptors.response.use(
+  response => {
+    const span = tracer.scope().active();
+    span.setTag('http.status_code', response.status);
+    span.finish();
+    return response;
   },
-};
+  error => {
+    const span = tracer.scope().active();
+    span.setTag('error', true);
+    span.finish();
+    return Promise.reject(error);
+  }
+);
 
-const tracer = initTracer(config, options);
-
-export default tracer;
+export default http;
