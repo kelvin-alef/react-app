d3d90dca59d6bcf095dff289c53371502efd5824
Kelvin Alef <kelvin.alefs@gmail.com>
2023-07-03 15:39:18 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import http from './tracing';
+import { initTracer } from 'jaeger-client';
 
-const { execSync } = require('child_process');
-execSync('npm install os-browserify');
+const tracer = initTracer({ serviceName: 'react-app' });
+
+const makeTracedRequest = async (method, url, config) => {
+  const span = tracer.startSpan(method.toUpperCase());
+  span.setTag('http.url', url);
+  span.setTag('http.method', method.toUpperCase());
+
+  try {
+    const response = await fetch(url, config);
+    span.setTag('http.status_code', response.status);
+    return response;
+  } catch (error) {
+    span.setTag('error', true);
+    throw error;
+  } finally {
+    span.finish();
+  }
+};
+
+const http = {
+  get: (url, config) => makeTracedRequest('GET', url, config),
+  post: (url, data, config) => makeTracedRequest('POST', url, { ...config, body: JSON.stringify(data) })
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
